2d667d3aa84662a1f39ae4d47218b80c44f5738f
Odafe Aror <odafe41@gmail.com>
2025-01-07 06:31:02 +0000

    fix 4

--- backend-api/src/app.ts
+++ backend-api/src/app.ts
@@ -30,8 +30,8 @@ app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Set-Cookie'],
-  exposedHeaders: ['Set-Cookie']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['set-cookie']
 }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -41,6 +41,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET || "siwe-quickstart-secret",
   resave: false,
   saveUninitialized: true,
+  proxy: true,
   store: MongoStore.create({
     mongoUrl: MONGODB_URI,
     collectionName: 'sessions'
